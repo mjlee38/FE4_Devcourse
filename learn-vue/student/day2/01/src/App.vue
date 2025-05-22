6bb379c6d963a231e88c120d971a2f71683761ac
mjlee <leeminji9938@naver.com>
2025-05-22 14:57:16 +0900

    ✏️ Note: v-model

---
+++ learn-vue/student/day2/01/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      gender: 'female',
+      className: 'a',
+    }
+  },
+}
+</script>
+<template>
+  <p>선택한 성별: {{ gender }}</p>
+  <p>선택한 반: {{ className }}</p>
+  <div>
+    <label><input type="radio" v-model="gender" name="gender" value="male" />남자</label>
+    <label><input type="radio" v-model="gender" name="gender" value="female" />여자</label>
+  </div>
+  <div>
+    <label><input type="radio" v-model="className" name="className" value="a" />A반</label>
+    <label><input type="radio" v-model="className" name="className" value="b" />B반</label>
+  </div>
+</template>
+<style scoped></style>
