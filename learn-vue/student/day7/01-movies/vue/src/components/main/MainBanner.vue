928b9d78fef92ff0df71da7194823b564a19a2d6
mjlee <leeminji9938@naver.com>
2025-05-28 12:41:22 +0900

    ✨ Loading main banner

--- learn-vue/student/day7/01-movies/vue/src/components/main/MainBanner.vue
+++ learn-vue/student/day7/01-movies/vue/src/components/main/MainBanner.vue
@@ -1,18 +1,34 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  movies: Array,
+  loading: Boolean,
+})
+
+const randomMovie = computed(() => {
+  if (!props.movies || props.movies.length === 0) return null
+  // const randomIndex = Math.floor(Math.random() * props.movies.length)
+  // return props.movies[randomIndex]
+
+  // overview 없는 영화 filter
+  const movieFilter = props.movies.filter((movie) => movie.overview && movie.overview.trim() !== '')
+  if (movieFilter.length === 0) return null
+  const randomIndex = Math.floor(Math.random() * movieFilter.length)
+  return movieFilter[randomIndex]
+})
+</script>
 <template>
-  <section class="release">
+  <section v-if="!props.loading && randomMovie" class="release">
     <div
       class="release-item"
-      style="
-        background-image: url('https://image.tmdb.org/t/p/w500//t5zCBSB5xMDKcDqe91qahCOUYVV.jpg');
-      "
+      :style="`background-image: url('https://image.tmdb.org/t/p/w500/${randomMovie.poster_path}');`"
     >
       <div class="release__text">
         <strong class="release__category">NEW RELEASE</strong>
-        <h2 class="release__title">Soviet : The Cold War</h2>
+        <h2 class="release__title">{{ randomMovie.title }}</h2>
         <p class="release__desc">
-          Her senses grow sharper, her thinking clearer, and for the first time in her life she
-          feels herself fully in control. By the age of sixteen, she's competing for the U.S.
+          {{ randomMovie.overview }}
         </p>
         <button class="release__btn">자세히보기</button>
       </div>
--- learn-vue/student/day7/01-movies/vue/src/views/HomeView.vue
+++ learn-vue/student/day7/01-movies/vue/src/views/HomeView.vue
@@ -24,7 +24,7 @@ onMounted(() => {
 })
 </script>
 <template>
-  <MainBanner />
+  <MainBanner :movies="movieUpcomingList" :loading="movieUpcomingListLoading" />
   <MainMovie />
 </template>
 <style scoped></style>
